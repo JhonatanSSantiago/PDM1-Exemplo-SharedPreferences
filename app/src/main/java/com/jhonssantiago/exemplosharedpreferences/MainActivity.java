90b8713213fe70447595c1bf28a37081e1a18e5d
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-04-04 11:42:32 -0300

    atividade 1

--- app/src/main/java/com/jhonssantiago/exemplosharedpreferences/MainActivity.java
+++ app/src/main/java/com/jhonssantiago/exemplosharedpreferences/MainActivity.java
@@ -3,10 +3,13 @@ package com.jhonssantiago.exemplosharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNome;
@@ -22,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clicar(View view){
-
-        Intent intent = new Intent(this, SegundaActivity.class);
-        intent.putExtra("nome_usuario",editTextNome.getText().toString());
-        startActivity(intent);
+        String nome = editTextNome.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("dados",MODE_PRIVATE);
+        String name = sharedPreferences.getString("nome_usuario", "");
+        Toast.makeText(this,"SP: "+name+" ET: "+nome,Toast.LENGTH_SHORT).show();
+        if(name.equals(nome)){
+            Intent intent = new Intent(this, TerceiraActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(this, SegundaActivity.class);
+            intent.putExtra("nome_usuario",nome);
+            startActivity(intent);
+        }
 
     }//
 }
